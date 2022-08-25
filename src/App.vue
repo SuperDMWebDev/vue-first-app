9ca5fb2b6664b56351c9809d40b7f290de5c6ced
minh <hadtnt71@gmail.com>
2022-08-25 09:11:34 +0700

    check

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>{{ title }}</h1>
   <p>Welcome...</p>
+
   <div v-if="showModal">
     <Modal theme="sale" @close="toggleModal">
       <template v-slot:links>
@@ -10,28 +11,46 @@
       <h1>Ninja Givaway!</h1>
       <p>Grab your ninja swag for half price!</p>
     </Modal>
+  </div>  
+
+  <div v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>Sign up to the Newsletter</h1>
+      <p>For updates and promo codes!</p>
+    </Modal>
   </div>
+
   <button @click.alt="toggleModal">open modal (alt click)</button>
+  <button @click="toggleModalTwo">open modal 2</button>
 </template>
 
 <script>
-import Modal from "./components/Modal";
+// challenge
+//   - create an extra button to open a different modal
+//   - use the same modal component but pass in a different template (slot)
+//   - use a different method (e.g. toggleModalTwo) and data (e.g. showModalTwo)
+
+import Modal from './components/Modal'
 
 export default {
-  name: "App",
+  name: 'App',
   components: { Modal },
   data() {
     return {
-      title: "My First Vue App!",
+      title: 'My First Vue App!',
       showModal: false,
-    };
+      showModalTwo: false,
+    }
   },
   methods: {
     toggleModal() {
-      this.showModal = !this.showModal;
+      this.showModal = !this.showModal
     },
-  },
-};
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
+    }
+  }
+}
 </script>
 
 <style>
@@ -47,5 +66,14 @@ h1 {
   border-bottom: 1px solid #ddd;
   display: inline-block;
   padding-bottom: 10px;
+}
+button {
+  background: #bbb;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  margin: 10px;
+  font-size: 16px;
+  color: #333;
 }
 </style>
