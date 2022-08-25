c8b4ce12be8f0547704cbbd0381279dc1f8bac01
minh <hadtnt71@gmail.com>
2022-08-25 08:55:53 +0700

    emit event

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <ModalItem
+      :heading="heading"
+      :text="text"
+      theme="sale"
+      @close="toggleModal"
+    />
+  </div>
+  <button @click.alt="toggleModal">open modal (alt click)</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ModalItem from "@/components/ModalIem.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { ModalItem },
+  data() {
+    return {
+      title: "My First Vue App!",
+      heading: "Sign up for the Givaway!",
+      text: "Grab your ninja swag for half price!",
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +42,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
